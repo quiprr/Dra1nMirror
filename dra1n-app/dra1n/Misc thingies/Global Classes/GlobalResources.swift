d306f0c178a2712fa15d40975a98454eb56dfa30
CharlieWhile13 <mrcharliewhile@gmail.com>
2020-11-23 16:10:59 +0000

    Big Ooof

---
+++ dra1n-app/dra1n/Misc thingies/Global Classes/GlobalResources.swift
@@ -0,0 +1,51 @@
+//
+//  GlobalResources.swift
+//  dra1n
+//
+//  Created by Charlie While on 05/08/2020.
+//
+
+import Foundation
+
+struct UpdateResponse: Decodable {
+    let UpdateAvailable: String!
+    let UpdateText: String!
+    let UpdateImage: String!
+    let isAnnouncement: Int?
+    let url: String!
+}
+
+struct postResponse: Decodable {
+    let Response: String!
+}
+
+struct boop: Decodable {
+    let purchased: Bool!
+}
+
+struct megaCoolPreCulprits: Decodable {
+    let Bundleid: String!
+    let warns: Int!
+    let flag: Int!
+}
+
+let sandboxed = false
+
+func endpoint() -> String {
+    if (sandboxed) {
+        return "sandbox"
+    } else {
+        return "api"
+    }
+}
+
+public var databaseURL: String {
+    #if staff
+    return "https://\(endpoint()).dra1n.app/v1/all/tweaks"
+    #else
+    return "https://\(endpoint()).dra1n.app/V1/tweaks/"
+    #endif
+}
+
+var leMtoTheB = false
+var leFishe = false
---
+++ dra1n-app/dra1n/Misc thingies/Table View Cells/Header Cell/headerCell.swift
@@ -0,0 +1,13 @@
+//
+//  headerCell.swift
+//  dra1n
+//
+//  Created by Charlie While on 14/08/2020.
+//
+
+import UIKit
+
+class headerCell: UITableViewCell {
+    @IBOutlet weak var info: UIButton!
+    @IBOutlet weak var clearLog: UIButton!
+}
